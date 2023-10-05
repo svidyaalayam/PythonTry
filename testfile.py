2748281939132f2a64ec9d6aaa0339b494e19323
IAS\Rama.Nemani <Rama.Nemani@iris.co.uk>
2023-10-05 23:24:19 +0100

    First Try

---
+++ testfile.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+tasks = []  # List to store tasks
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        task = request.form['task']
+        tasks.append(task)
+    return render_template('index.html', tasks=tasks)
+
+if __name__ == '__main__':
+    app.run(debug=True)
